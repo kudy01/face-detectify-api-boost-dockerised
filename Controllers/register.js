61c2874bccf16e73a8f9b76b0673982dd3c5c8f1
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-11-07 13:50:11 +0530

    Added session management for register end point

--- Controllers/register.js
+++ Controllers/register.js
@@ -1,14 +1,17 @@
-
+const jwt = require('jsonwebtoken');
+const redis = require("redis");
+//SET UP REDIS
+const redisClient = redis.createClient(process.env.REDIS_URI);
 
 const handleRegister = (req, res, db, bcrypt) =>{
-	const {email, name, password} = req.body;
+	const {email, name, password} = req.body; 
 	if(!email || !name || !password){
 				
-		return res.status(400).json('Incorrect form of submission')
+		return Promise.reject('Incorrect form of submission')
 	}
 	const hash = bcrypt.hashSync(password);
 		// starting a transaction as we are doing more than two things, namely inserting info into the users and login table as both should contain the details of a new user
-		db.transaction(trx => {
+		return db.transaction(trx => {
 			trx.insert({
 				hash: hash,
 				email: email
@@ -23,16 +26,59 @@ const handleRegister = (req, res, db, bcrypt) =>{
 						name: name,
 						joined: new Date()
 					})
-					.then(user => {
-						res.json(user[0])
-					})
+					.then(user => user[0])
 			})
-			.then(trx.commit)
+			.then(trx.commit) 
 			.catch(trx.rollback)
 		})
-		.catch(err => res.status(400).json('unable to register'))
+		.catch(err => Promise.reject('Unable to get Register'))
 }
 
+const getAuthTokenId = (req, res) => {
+	const { authorization } = req.headers;
+	return redisClient.get(authorization, (err, reply) => {
+		if(err || !reply) {
+			return res.status(400).json('Unauthorised')
+		}
+		return res.json({id: reply})
+	})
+}
+
+const registerToken = (email) => {
+	const jwtPayload = { email };
+	return jwt.sign(jwtPayload, 'JWT_secret', { expiresIn: '7 days'})
+}
+
+const setToken = (key, value) => {
+	return Promise.resolve(redisClient.set(key, value))
+}
+
+const createSession = (user) => {
+	//create jwt token and return user info 
+	const { email, id } = user;
+	const token = registerToken(email)
+	return setToken(token, id)
+		.then(()=> {
+			return { success: 'true', userId: id, token: token }
+		})
+		.catch(err=> res.status(400).json('Invalid Email or Password'))
+}
+
+const registerAuthentication = (db, bcrypt) => (req, res) => { //higher order as it returns another function
+	const { authorization } = req.headers;
+	return authorization &&
+		handleRegister(req, res, db, bcrypt) 
+		.then(user => {
+			console.log(user.id, user.email)
+			return user.id && user.email ?
+			createSession(user) : Promise.reject(user)
+		})
+		.then(session => res.json(session))
+		.catch(err=> res.status(400).json('Invalid Email or Password')) 
+	
+} 
+
 module.exports = {
-	handleRegister: handleRegister
+	handleRegister: handleRegister,
+	registerAuthentication: registerAuthentication
 };
